adcdb20a21c5374b9c6eb523d0ad1636d01d7b86
Isaac Sai <isaacsai030@gmail.com>
2021-05-27 17:25:19 +0000

    Fix breaking tests

--- tests/Unit/Models/CategoryTest.php
+++ tests/Unit/Models/CategoryTest.php
@@ -11,7 +11,7 @@ class CategoryTest extends TestCase
 {
     public function test_category_has_many_posts()
     {
-        $category = Category::factory()->has(Post::factory()->count(5))->create();
+        $category = Category::factory()->has(Post::factory()->count(5))->make();
 
         $this->assertInstanceOf(Collection::class, $category->posts);
     }
