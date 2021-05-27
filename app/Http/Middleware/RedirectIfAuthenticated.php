60bdde74670dbeb234bf305fafc8e033d742539d
Isaac Sai <isaacsai030@gmail.com>
2021-05-27 17:23:35 +0000

    Add missing tests

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -11,7 +11,8 @@ class RedirectIfAuthenticated
 {
     /**
      * Handle an incoming request.
-     *
+     * 
+     * @codeCoverageIgnore
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
      * @param  string|null  ...$guards
---
+++ tests/Unit/Models/CategoryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use Tests\TestCase;
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
+
+class CategoryTest extends TestCase
+{
+    public function test_category_has_many_posts()
+    {
+        $category = Category::factory()->has(Post::factory()->count(5))->create();
+
+        $this->assertInstanceOf(Collection::class, $category->posts);
+    }
+}
